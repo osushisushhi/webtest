4c1899571a977cfe08afacd3fba0be3268f1b430
osushisushhi <testsushhi@outlook.jp>
2024-12-20 15:12:12 +0900

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,12 @@ document.getElementById('myForm').addEventListener('submit', function(event) {
   const name = document.getElementById('name').value;
   const message = document.getElementById('message').value;
 
+  // 入力チェック
+  if (!name || !message) {
+    alert('名前とメッセージを入力してください');
+    return;
+  }
+
   // Webhook URLを指定
   const webhookURL = 'https://discord.com/api/webhooks/1319532905340473384/OHk4MNGfsknfs9KyJ2QjkcZBizcxa4zTTOWQ9BvsE2F-ROOFf1Jb1naUhFugWg0_GZI3'; // ここにWebhook URLを貼り付け
 
