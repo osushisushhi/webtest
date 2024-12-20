2ef5b233c4615955b461cba1c4d9fa1dbd29ff81
osushisushhi <testsushhi@outlook.jp>
2024-12-20 15:24:41 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-fetch('', {
+fetch('https://discord.com/api/webhooks/1319532905340473384/OHk4MNGfsknfs9KyJ2QjkcZBizcxa4zTTOWQ9BvsE2F-ROOFf1Jb1naUhFugWg0_GZI3', {
   method: 'POST',
   headers: { 'Content-Type': 'application/json' },
   body: JSON.stringify({ content: 'テストメッセージ' })
